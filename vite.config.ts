11c8f1b860cbf0fb12e0b3ed84fb37527241f913
Mac <mac@Macs-MacBook-Pro.local>
2024-01-06 18:15:00 +0530

    Stories customisation in theme app extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,20 @@
-import { defineConfig ,loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
-// https://vitejs.dev/config/
-export default defineConfig(  ({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.NODE_ENV': JSON.stringify("production")
+export default defineConfig({
+  define: {
+    "process.env.NODE_ENV": JSON.stringify("production"),
+  },
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    modulePreload: false,
+    lib: {
+      entry: resolve(__dirname, "src/main.tsx"),
+      name: "stories",
+      formats: ["iife"],
+      fileName: "stories",
     },
-    plugins: [react(), cssInjectedByJsPlugin()],
-    build: {
-      modulePreload: false,
-      lib: {
-        // Could also be a dictionary or array of multiple entry points
-        entry: resolve(  __dirname   , "src/main.tsx"),
-        name: "f22",
-        formats: ["iife"],
-        // the proper extensions will be added
-        fileName: "f22",
-      },
-    },
-  } 
-
+  },
 });
